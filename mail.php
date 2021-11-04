55193c6f72f61b1c1900cf66746ca72fce4dd635
pashahohlov <80853130+pashahohlov@users.noreply.github.com>
2021-11-04 23:44:30 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,30 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+
+$mail->isSMTP();
+$mail->Host = 'smtp.mail.ru';
+$mail->SMTPAuth = true;
+$mail->Username = 'hohlov1106@mail.ru';
+$mail->Password = 'gfif1106';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+$mail->setFrom('hohlov1106@mail.ru');
+$mail->addAddress('pashahohlov54@gmail.com');
+
+$mail->isHTML(true);
+
+$mail->Subject = '';
+$mail->Body    = 'Нажата кнопка';
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: sent.html');
+}
+?>
